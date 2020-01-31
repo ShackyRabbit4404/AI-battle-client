3c9f5b7d9c18f1c1d564a9cd22fb0fe5082c8602
Brian Hulbert <brian.hulbert@me.com>
2020-01-31 14:07:12 -0500

    Baisc setup of the GUI

--- Main.java
+++ Main.java
@@ -2,15 +2,53 @@ import javax.swing.*;
 import java.awt.*;
 public class Main{
     public static void main(String[] args){
-        JFrame frame = new JFrame("Battle Client");
+        JFrame frame = new JFrame("AI Battle Client");
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setBounds(0,0,screenSize.width,screenSize.height);
         JPanel panel = new JPanel(new BorderLayout());
-        JPanel control = new JPanel();
-        JButton test = new JButton("TEST");
-        control.add(test);
-        panel.add(control,BorderLayout.WEST);
+        String[] ais = new String[]{"Bob","Jeff","Steve","Martin"};
         
+        JPanel control = new JPanel();
+        control.setBackground(Color.GRAY);
+        JPanel row1 = new JPanel();
+        JLabel hello = new JLabel("AI BATTLE CLIENT");
+        row1.add(hello);
+        row1.setBackground(Color.WHITE);
+        
+        JPanel row2 = new JPanel();
+        JLabel gameName = new JLabel("Select Game File: ");
+        String[] gameTypes = new String[]{"Chess","Checkers","Battleship","Tic-Tac-Toe"};
+        JComboBox games = new JComboBox(gameTypes);
+        row2.add(gameName);
+        row2.add(games);
+        row2.setBackground(Color.WHITE);
+        
+        JPanel row3 = new JPanel();
+        JLabel aiOneLabel = new JLabel("Player 1: ");
+        JComboBox aiOneChoice = new JComboBox(ais);
+        row3.add(aiOneLabel);
+        row3.add(aiOneChoice);
+        row3.setBackground(Color.WHITE);
+        
+        JPanel row4 = new JPanel();
+        JLabel aiTwoLabel = new JLabel("Player 2: ");
+        JComboBox aiTwoChoice = new JComboBox(ais);
+        row4.add(aiTwoLabel);
+        row4.add(aiTwoChoice);
+        row4.setBackground(Color.WHITE);
+        
+        JPanel row5 = new JPanel();
+        JButton run = new JButton("Run Game");
+        row5.add(run);
+        row5.setBackground(Color.WHITE);
+        
+        control.add(row1);
+        control.add(row2);
+        control.add(row3);
+        control.add(row4);
+        control.add(row5);
+        
+        panel.add(control,BorderLayout.NORTH);
         
         Display display = new Display();
         panel.add(display, BorderLayout.CENTER);
