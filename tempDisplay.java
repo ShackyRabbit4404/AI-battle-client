13711264fc7d6e6266cbd1e1de27ca6eb7fc039d
Brian Hulbert <brian.hulbert@me.com>
2020-02-11 14:13:57 -0500

    worked on  the temp display class

--- tempDisplay.java
+++ tempDisplay.java
@@ -5,7 +5,22 @@ public class tempDisplay extends JPanel{
     public tempDisplay(){
         map = new int[10][10];
     }
+    public void randomizeMap(){
+        
+    }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
+        for(int x = 0; x < map.length;x++){
+            for(int y = 0; y < map[0].length; y++){
+                if(map[x][y] == 0){
+                    g.setColor(Color.BLUE);
+                    g.fillRect(x*20,y*20,20,20);
+                }
+                else{
+                    g.setColor(Color.RED);
+                    g.fillRect(x*20,y*20,20,20);
+                }
+            }
+        }
     }
 }
